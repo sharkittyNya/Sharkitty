22cccc8c565d936c39b6c69de7049e5463e0d5a6
Il Harper <hi@ilharper.com>
2023-08-25 21:12:52 +0800

    fix(core): fix message send

---
+++ packages/core/src/routes/message/send.ts
@@ -0,0 +1,25 @@
+import type { MessageSendPayload } from '@chronocat/red'
+import { sendMsg } from '../../ipc/definitions/msgService'
+import { router } from '../../router'
+import { uixCache } from '../../uixCache'
+import { filterMessage } from '../../utils/filterMessage'
+import { makeFullPacket } from '../../utils/packetHelper'
+
+router.message.send.$body('json')(async ({ body }) => {
+  const payload = body as MessageSendPayload
+
+  if (!filterMessage(payload)) return {}
+
+  makeFullPacket(payload as unknown as Record<string, unknown>)
+
+  await sendMsg({
+    msgId: '0',
+    peer: await uixCache.preprocessObject(payload.peer),
+    msgElements: await uixCache.preprocessObject(payload.elements, {
+      contextGroup:
+        payload.peer.chatType === 2 ? Number(payload.peer.peerUin) : undefined,
+    }),
+  })
+
+  return {}
+})
