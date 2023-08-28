52969d4021571032672470e1b57ae36ce830bb65
Il Harper <hi@ilharper.com>
2023-08-28 16:14:43 +0800

    fix(core): fix upload api (#3)
    * fix(core): fix upload api
    
    当前版本代码未等待文件上传完毕就已返回，导致通用响应更早被触发，最终用户无法得到
    Media 对象。
    
    修改后的代码在 Media 对象响应后手动调用 response，解决了这一问题。
    
    * fix(core): resolve review 1597069723
    
    https://github.com/chrononeko/chronocat/pull/3#pullrequestreview-1597069723
    
    * fix(core): handle busboy error
    
    * fix(core): fix upload async timing
    
    * fix(core): fix upload body
    
    * fix(core): do not write head in route
    
    * fix(core): remove stringify

--- packages/core/src/routes/upload.ts
+++ packages/core/src/routes/upload.ts
@@ -1,6 +1,5 @@
 import busboy from 'busboy'
 import { randomFillSync } from 'node:crypto'
-import type { PathLike } from 'node:fs'
 import { createWriteStream } from 'node:fs'
 import { copyFile, mkdir } from 'node:fs/promises'
 import { join } from 'node:path'
@@ -14,77 +13,81 @@ import { getRichMediaFilePath } from '../ipc/definitions/msgService'
 import { router } from '../router'
 import { baseDir } from '../utils/baseDir'
 
-router.upload.$body('binary').$httpOnly('POST')(
-  async ({ http: { req, res } }) => {
-    const bb = busboy({ headers: req.headers })
-    let filePath: PathLike
-    let fileInfo: busboy.FileInfo
+router.upload.$httpOnly('POST')(
+  ({ http: { req, res } }) =>
+    new Promise((resolve, reject) => {
+      const bb = busboy({ headers: req.headers })
 
-    bb.on(
-      'file',
-      (_name, file, info) =>
-        void (async () => {
+      let f:
+        | Promise<{
+            filePath: string
+            fileInfo: busboy.FileInfo
+          }>
+        | undefined
+
+      bb.on('error', reject)
+
+      bb.on('file', (_name, file, info) => {
+        f = (async () => {
           const saveTo = join(baseDir, `redprotocol-upload`)
           await mkdir(saveTo, { recursive: true })
-          filePath = join(
+          const filePath = join(
             saveTo,
             `${randomFillSync(Buffer.alloc(16)).toString('hex')}-${
               info.filename
             }`,
           )
-          fileInfo = info
           file.pipe(createWriteStream(filePath))
-        })(),
-    )
-
-    bb.on(
-      'close',
-      () =>
-        void (async () => {
-          if (!filePath) {
-            res.writeHead(400)
-            res.end(`400 bad request`)
-            return
+          return {
+            filePath,
+            fileInfo: info,
           }
+        })()
+      })
 
-          const fileType: {
-            mime: string
-          } = await getFileType(filePath)
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      bb.on('close', async () => {
+        if (!f) {
+          res.writeHead(400)
+          res.end(`400 bad request`)
+          return
+        }
 
-          const category = fileType.mime.split('/')[0]
+        const { filePath, fileInfo } = await f
 
-          const [md5, imageInfo, fileSize] = await Promise.all([
-            getFileMd5(filePath),
-            category === 'image' ? getImageSizeFromPath(filePath) : undefined,
-            getFileSize(filePath),
-          ])
+        const fileType: {
+          mime: string
+        } = await getFileType(filePath)
 
-          const richMediaPath = await getRichMediaFilePath({
-            md5HexStr: md5,
-            fileName: fileInfo.filename,
-            elementType: 2,
-            elementSubType: 0,
-            thumbSize: 0,
-            needCreate: true,
-            fileType: 1,
-          })
+        const category = fileType.mime.split('/')[0]
 
-          await copyFile(filePath, richMediaPath as string)
+        const [md5, imageInfo, fileSize] = await Promise.all([
+          getFileMd5(filePath),
+          category === 'image' ? getImageSizeFromPath(filePath) : undefined,
+          getFileSize(filePath),
+        ])
 
-          res.writeHead(200)
+        const richMediaPath = await getRichMediaFilePath({
+          md5HexStr: md5,
+          fileName: fileInfo.filename,
+          elementType: 2,
+          elementSubType: 0,
+          thumbSize: 0,
+          needCreate: true,
+          fileType: 1,
+        })
 
-          res.end(
-            JSON.stringify({
-              md5,
-              imageInfo,
-              fileSize,
-              filePath,
-              ntFilePath: richMediaPath,
-            }),
-          )
-        })(),
-    )
+        await copyFile(filePath, richMediaPath as string)
 
-    req.pipe(bb)
-  },
+        resolve({
+          md5,
+          imageInfo,
+          fileSize,
+          filePath,
+          ntFilePath: richMediaPath,
+        })
+      })
+
+      req.pipe(bb)
+    }),
 )
