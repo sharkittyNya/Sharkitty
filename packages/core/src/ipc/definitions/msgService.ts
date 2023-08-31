c356d72b27ee0a5f116cb926a2da22e6610bdb1c
Il Harper <hi@ilharper.com>
2023-09-01 00:23:05 +0800

    chore: lint

--- packages/core/src/ipc/definitions/msgService.ts
+++ packages/core/src/ipc/definitions/msgService.ts
@@ -1,5 +1,5 @@
 import type { Element, Peer } from '@chronocat/red'
-import { Object } from 'ts-toolbelt'
+import type { Object } from 'ts-toolbelt'
 import { defineIpcApi } from '../define'
 
 export const recallMsg = defineIpcApi<
