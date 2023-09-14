d6d908c36e02db3d8723b9e7bcfdb3825c787db5
Il Harper <hi@ilharper.com>
2023-09-15 03:10:58 +0800

    refactor(red): update type `UploadResponse`

--- packages/red/src/types.ts
+++ packages/red/src/types.ts
@@ -105,7 +105,7 @@ export interface MessageSendForwardPayload {
 
 export interface UploadResponse {
   md5: string
-  imageInfo: {
+  imageInfo?: {
     width: number
     height: number
     type: string // png
