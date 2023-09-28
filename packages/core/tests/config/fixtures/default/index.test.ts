7a8cb65d22b51499107c6c2c74a841009ea274cc
Il Harper <hi@ilharper.com>
2023-09-28 16:25:36 +0800

    test(core): update config test

--- packages/core/tests/config/fixtures/default/index.test.ts
+++ packages/core/tests/config/fixtures/default/index.test.ts
@@ -10,7 +10,9 @@ test('配置文件解析器应当正确解析 默认配置文件', async () => {
   const raw = (await readFile(join(__dirname, 'chronocat.yml'))).toString()
   const config = load(raw)
 
+  // parseConfig 一参是传入的 chronocat.yml，二参是当前登录的 QQ 号
   const parsed1 = parseConfig(config, '1')
+  // 当前登录的 QQ 是 1，1 这个 QQ 在 overrides 里没有找见，于是使用默认设置
   expect(parsed1).toEqual<ChronocatCurrentConfig>({
     enable: true,
     servers: [
@@ -31,6 +33,11 @@ test('配置文件解析器应当正确解析 默认配置文件', async () => {
     ],
   })
 
+  // 当前登录的 QQ 是 10000
+  // 10000 这个 QQ 在 overrides 里有
+  // 那么，overrides 定义的每一个属性都会直接覆盖掉根对象的属性
+  // chronocat.yml 里，10000 下面覆写了 servers 这个属性
+  // 因此根对象的 servers 设置就被抛弃了，直接使用覆写后的 servers，所以端口是 16531 和 5501
   const parsed10000 = parseConfig(config, '10000')
   expect(parsed10000).toEqual<ChronocatCurrentConfig>({
     enable: true,
