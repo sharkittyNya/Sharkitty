4dd9a2e7be7d988ffcdb46dbd30d0150a90324fe
Il Harper <hi@ilharper.com>
2023-09-30 14:56:29 +0800

    docs: update docs

--- packages/docs/src/components/Constructing.tsx
+++ packages/docs/src/components/Constructing.tsx
@@ -11,7 +11,7 @@ export const Constructing = () => (
         此页面仍在施工中！<br></br>
         如果你希望我们尽快完成此页面，可以通过
         <Link to="/more/community">社区</Link>
-        途径反馈给我们。
+        途径反馈给我们，我们会优先编写你需要的内容！
       </p>
     </Admonition>
   </div>
--- packages/docs/src/components/UntrustedContents.tsx
+++ packages/docs/src/components/UntrustedContents.tsx
@@ -8,8 +8,8 @@ export const UntrustedContents = () => (
   <div>
     <Admonition type="caution">
       <p>
-        下面的一些内容不由 Chronocat
-        管理，你应当前往对应项目以获得最新、最详细的教程。<br></br>
+        下面的一些内容不由 Chrononeko
+        团队管理，你应当前往对应项目以获得最新、最详细的教程。<br></br>
         如果你遇到了任何问题，你都可以通过
         <Link to="/more/community">社区</Link>
         途径寻求帮助。
