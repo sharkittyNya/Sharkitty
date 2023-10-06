ab3c218869bcec95ca323b5dfa4f94520a972b35
Il Harper <hi@ilharper.com>
2023-10-07 00:46:24 +0800

    feat(core): satori: api `message.create` check params

--- packages/core/src/satori/routes/message/create.ts
+++ packages/core/src/satori/routes/message/create.ts
@@ -3,7 +3,18 @@ import { Messager } from '../../messager'
 import type { MessageCreatePayload } from '../../types'
 import type { RouteContext } from '../types'
 
-export const messageCreate = async ({ config, json }: RouteContext) => {
+export const messageCreate = async ({ config, res, json }: RouteContext) => {
   const { channel_id, content } = (await json()) as MessageCreatePayload
+
+  if (
+    !channel_id ||
+    !content ||
+    (content && Array.isArray(content) && !content.length)
+  ) {
+    res.writeHead(400)
+    res.end('400 bad request')
+    return
+  }
+
   return new Messager(config, common, channel_id).send(content)
 }
