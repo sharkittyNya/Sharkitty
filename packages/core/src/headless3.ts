8ff6a68395fb6bdd9c5a763396556f2313ee584c
Il Harper <hi@ilharper.com>
2023-10-06 20:15:43 +0800

    feat(core): headless: update headless 3

--- packages/core/src/headless3.ts
+++ packages/core/src/headless3.ts
@@ -34,7 +34,7 @@ export const initHeadless3 = () => {
 
     if (!app.isReady) app.disableHardwareAcceleration()
 
-    let handleClearWindowsTimer: NodeJS.Timeout | undefined = undefined
+    // let handleClearWindowsTimer: NodeJS.Timeout | undefined = undefined
 
     const FakeBrowserWindow = new Proxy(BrowserWindow, {
       construct(
@@ -43,65 +43,67 @@ export const initHeadless3 = () => {
       ) {
         args[0].width = 3
         args[0].height = 3
-        console.log('[headless] ConstructBW', args[0])
+        // console.log('[headless] ConstructBW', args[0])
 
-        if (!args[0].title && !handleClearWindowsTimer) {
-          app.removeAllListeners('window-all-closed')
-          handleClearWindowsTimer = setInterval(() => {
-            if (!globalThis.authData) return
+        // if (!args[0].title && !handleClearWindowsTimer) {
+        //   app.removeAllListeners('window-all-closed')
+        //   handleClearWindowsTimer = setInterval(() => {
+        //     if (!globalThis.authData) return
 
-            setTimeout(() => {
-              BrowserWindow.getAllWindows().forEach((v) => {
-                v.removeAllListeners()
+        //     setTimeout(() => {
+        //       BrowserWindow.getAllWindows().forEach((v) => {
+        //         v.removeAllListeners()
 
-                // v.close()
+        //         // v.close()
 
-                try {
-                  // @ts-expect-error BaseWindow 没有 dts
-                  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
-                  require('electron').BaseWindow.prototype.destroy.call(v)
-                } catch (e) {
-                  console.log('headless3: destroy failed: ', e)
-                }
-              })
-            }, 5000)
-          }, 5000)
-        }
+        //         try {
+        //           // @ts-expect-error BaseWindow 没有 dts
+        //           // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
+        //           require('electron').BaseWindow.prototype.destroy.call(v)
+        //         } catch (e) {
+        //           console.log('headless3: destroy failed: ', e)
+        //         }
+        //       })
+        //     }, 5000)
+        //   }, 5000)
+        // }
 
-        type BWKeys = Exclude<keyof BrowserWindow, 'id' | 'webContents'>
-        type BWFunctions = BrowserWindow[BWKeys]
+        // type BWKeys = Exclude<keyof BrowserWindow, 'id' | 'webContents'>
+        // type BWFunctions = BrowserWindow[BWKeys]
 
         const win = new BrowserWindow(...args)
-        const winOriginMethods = {} as Record<BWKeys, BWFunctions>
+        // const winOriginMethods = {} as Record<BWKeys, BWFunctions>
 
-        for (const i in win) {
-          const ii = i as BWKeys
+        // for (const i in win) {
+        //   const ii = i as BWKeys
 
-          if (typeof win[ii] === 'function') {
-            winOriginMethods[ii] = win[ii] as () => void // .bind(win)
+        //   if (typeof win[ii] === 'function') {
+        //     winOriginMethods[ii] = win[ii] as () => void // .bind(win)
 
-            win[ii] = ((...args: unknown[]) => {
-              console.log('[headless3] win called ', ii, args)
-              if (ii === 'isDestroyed') {
-                console.log(
-                  '[headless3] win called isDestroyed ',
-                  new Error().stack,
-                )
-                return false
-              }
+        //     win[ii] = ((...args: unknown[]) => {
+        //       console.log('[headless3] win called ', ii, args)
+        //       if (ii === 'isDestroyed') {
+        //         console.log(
+        //           '[headless3] win called isDestroyed ',
+        //           new Error().stack,
+        //         )
+        //         return false
+        //       }
 
-              return (
-                winOriginMethods[ii] as (...p: unknown[]) => unknown
-              ).apply(win, args)
-            }) as never
-          }
-        }
+        //       return (
+        //         winOriginMethods[ii] as (...p: unknown[]) => unknown
+        //       ).apply(win, args)
+        //     }) as never
+        //   }
+        // }
 
         win.webContents.setFrameRate(1)
-        win.webContents.on('paint', () => {})
+        win.webContents.on('paint', ({ preventDefault }) => preventDefault())
         win.setSize = () => {}
+        win.setMinimumSize = () => {}
+        win.setMaximumSize = () => {}
         win.setPosition = () => {}
-        win.show = () => console.log('[headless] Not showing window')
+        win.show = () => {}
         return win
       },
     })
