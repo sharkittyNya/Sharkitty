d6364a2a1fe89d96f27ea009fdbbf1c6878a1106
Il Harper <hi@ilharper.com>
2023-10-06 13:56:20 +0800

    refactor(core): login: use `console.log`

--- packages/core/src/loginService.ts
+++ packages/core/src/loginService.ts
@@ -64,7 +64,7 @@ export const initLoginService = () => {
       host = '0.0.0.0'
     }
 
-    console.warn(
+    console.log(
       `访问此链接以登录：\nhttp://127.0.0.1:${port}/login#127.0.0.1:${port}@${token}`,
     )
 
