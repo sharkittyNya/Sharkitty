4b0ea13b50e95a35d8f92e00b94cffcf203d04ad
Il Harper <hi@ilharper.com>
2023-10-17 00:59:10 +0800

    test(core): add parser tests

---
+++ packages/core/tests/common/index.ts
@@ -0,0 +1,9 @@
+export const satoriConfig = {
+  type: 'satori',
+  listen: '0.0.0.0',
+  port: 5500,
+  platform: 'chronocat',
+  self_url: 'https://chronocat.vercel.app',
+  token: 'DEFINE_CHRONO_TOKEN',
+  enable: true,
+} as const
---
+++ packages/core/tests/parser/fixtures/f-2-1-0-0/index.test.ts
@@ -0,0 +1,19 @@
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import type { Message, WsPackage } from '../../../../src/red'
+import { buildParser } from '../../../../src/satori/parser'
+import { satoriConfig } from '../../../common'
+
+test('Satori解析器应当正确解析 纯文本消息', async () => {
+  const data = JSON.parse(
+    (await readFile(join(__dirname, 'data.json'))).toString('utf-8'),
+  ) as WsPackage<Message[]>
+
+  expect(data.payload).toHaveLength(1)
+
+  const [message] = data.payload
+  const events = await buildParser('9999', satoriConfig)(message)
+
+  expect(events).toBeTruthy() // 解析到的消息应该存在
+  expect(events).toHaveLength(1) // 应当解析到 1 条消息
+})
