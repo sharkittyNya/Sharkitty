b8415429484b63f14cbcc9c88897d86b89ba1cef
Il Harper <hi@ilharper.com>
2023-10-17 19:12:17 +0800

    test(core): use shapshots

--- packages/core/tests/config/fixtures/default/index.test.ts
+++ packages/core/tests/config/fixtures/default/index.test.ts
@@ -2,7 +2,6 @@ import { load } from 'js-yaml'
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { parseConfig } from '../../../../src/config/parser'
-import type { ChronocatCurrentConfig } from '../../../../src/config/types'
 
 test('配置文件解析器应当正确解析 默认配置文件', async () => {
   expect.assertions(2)
@@ -13,27 +12,7 @@ test('配置文件解析器应当正确解析 默认配置文件', async () => {
   // parseConfig 一参是传入的 chronocat.yml，二参是当前登录的 QQ 号
   const parsed1 = parseConfig(config, '1')
   // 当前登录的 QQ 是 1，1 这个 QQ 在 overrides 里没有找见，于是使用默认设置
-  expect(parsed1).toEqual<ChronocatCurrentConfig>({
-    enable: true,
-    servers: [
-      {
-        type: 'red',
-        listen: '0.0.0.0',
-        port: 16530,
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-      {
-        type: 'satori',
-        listen: '0.0.0.0',
-        platform: 'chronocat',
-        port: 5500,
-        self_url: 'https://chronocat.vercel.app',
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-    ],
-  })
+  expect(parsed1).toMatchSnapshot()
 
   // 当前登录的 QQ 是 10000
   // 10000 这个 QQ 在 overrides 里有
@@ -41,25 +20,5 @@ test('配置文件解析器应当正确解析 默认配置文件', async () => {
   // chronocat.yml 里，10000 下面覆写了 servers 这个属性
   // 因此根对象的 servers 设置就被抛弃了，直接使用覆写后的 servers，所以端口是 16531 和 5501
   const parsed10000 = parseConfig(config, '10000')
-  expect(parsed10000).toEqual<ChronocatCurrentConfig>({
-    enable: true,
-    servers: [
-      {
-        type: 'red',
-        listen: '0.0.0.0',
-        port: 16531,
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-      {
-        type: 'satori',
-        listen: '0.0.0.0',
-        platform: 'chronocat',
-        port: 5501,
-        self_url: 'https://chronocat.vercel.app',
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-    ],
-  })
+  expect(parsed10000).toMatchSnapshot()
 })
--- packages/core/tests/config/fixtures/override/index.test.ts
+++ packages/core/tests/config/fixtures/override/index.test.ts
@@ -2,7 +2,6 @@ import { load } from 'js-yaml'
 import { readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { parseConfig } from '../../../../src/config/parser'
-import type { ChronocatCurrentConfig } from '../../../../src/config/types'
 
 test('配置文件解析器应当正确解析 被覆写的默认值', async () => {
   expect.assertions(2)
@@ -11,49 +10,9 @@ test('配置文件解析器应当正确解析 被覆写的默认值', async () =
   const config = load(raw)
 
   const parsed1 = parseConfig(config, '1')
-  expect(parsed1).toEqual<ChronocatCurrentConfig>({
-    enable: false,
-    servers: [
-      {
-        type: 'red',
-        listen: '0.0.0.0',
-        port: 16530,
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-      {
-        type: 'satori',
-        listen: '0.0.0.0',
-        platform: 'chronocat',
-        port: 5500,
-        self_url: 'https://chronocat.vercel.app',
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-    ],
-  })
+  expect(parsed1).toMatchSnapshot()
 
   const parsed10000 = parseConfig(config, '10000')
-  expect(parsed10000).toEqual<ChronocatCurrentConfig>({
-    // 虽然 enable 的默认值为 true，但由于 10000 账号的配置并未覆写 enable，所以此值仍应为 false
-    enable: false,
-    servers: [
-      {
-        type: 'red',
-        listen: '0.0.0.0',
-        port: 16531,
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-      {
-        type: 'satori',
-        listen: '0.0.0.0',
-        platform: 'chronocat',
-        port: 5501,
-        self_url: 'https://chronocat.vercel.app',
-        token: 'DEFINE_CHRONO_TOKEN',
-        enable: true,
-      },
-    ],
-  })
+  // 虽然 enable 的默认值为 true，但由于 10000 账号的配置并未覆写 enable，所以解析得到的 enable 仍应为 false
+  expect(parsed10000).toMatchSnapshot()
 })
